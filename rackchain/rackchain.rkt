bcf8ac0ebde1d085c5e5fe076d9dbc782d323314
Andrea <a.g.baldan@gmail.com>
2019-10-11 18:28:39 +0200

    Add proof of work

--- rackchain/rackchain.rkt
+++ rackchain/rackchain.rkt
@@ -2,6 +2,8 @@
 
 (require file/sha1)
 
+(define TARGET-BITS 16)
+
 (struct block (timestamp data prev-block hashcode) #:transparent)
 (struct blockchain (blocks))
 
@@ -9,16 +11,35 @@
   (bytes->hex-string
    (sha256-bytes
     (open-input-string
-     (string-append prev-hash (string-append data (number->string timestamp)))))))
+     (string-append* prev-hash data (list (number->string timestamp)))))))
+
+(define (proof-of-work block nonce)
+  (let* ([data (string-append*
+                (block-prev-block block)
+                (list
+                 (block-data block)
+                 (number->string (block-timestamp block))
+                 (number->string TARGET-BITS)
+                 (number->string nonce)))]
+         [target (arithmetic-shift 1 (- 256 TARGET-BITS))]
+         [data-hash (string->number
+                     (bytes->hex-string
+                      (sha256-bytes (open-input-string data))) 16)])
+    (cond
+      [(< data-hash target) (cons (number->string data-hash 16) nonce)]
+      [else (proof-of-work block (+ 1 nonce))])))
 
 (define (get-block data prev-block-hash)
   (let* ([now (current-seconds)]
-         [hashcode (hash-block prev-block-hash data now)])
+         ;[hashcode (hash-block prev-block-hash data now)])
+         [new-block (block now data prev-block-hash "")]
+         [hashcode (car (proof-of-work new-block 0))])
     (block now data prev-block-hash hashcode)))
 
 (define (add-block bc data)
   (let* ([prev (last (blockchain-blocks bc))]
          [new-block (get-block data (block-hashcode prev))])
+    (displayln (format "Mining the block ~s" data))
     (blockchain (append (blockchain-blocks bc) (list new-block)))))
 
 (define (genesis-block)
@@ -33,4 +54,8 @@
               (add-block (new-blockchain) "Send 1 BTC to Casper")
               "Send 5 BTC to Melchior")
              "Send 2 BTC to Balthasar")])
-    (for-each displayln (blockchain-blocks bc))))
+    (for-each
+     (λ (block)
+       (displayln (format "Block: ~s" (block-data block)))
+       (displayln (format "Hash: ~s" (block-hashcode block))))
+     (blockchain-blocks bc))))
